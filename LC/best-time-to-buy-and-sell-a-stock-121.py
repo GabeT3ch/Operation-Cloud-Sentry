7983fb68e5269771be868fc6e48ac72642f72d9d
Gabriel <gabriel.henderson173089@gmail.com>
2026-06-19 12:15:36 -0500

    LC 121 completed this problem helped me when it came to using programming in a real world use case and how the logic translates in real life

--- LC/best-time-to-buy-and-sell-a-stock-121.py
+++ LC/best-time-to-buy-and-sell-a-stock-121.py
@@ -4,15 +4,15 @@ class Solution:
 
 
     def Maximum_profit(self,prices):
-        min_price = prices[0]
-        max_profit = 0
+        min_price = prices[0] #start by assuming the first price is the cheapest
+        max_profit = 0 # no profit yet we start at zero
 
 
         for price in prices:
-            if price  < min_price:
+            if price  < min_price: #this is saying if the price is less than the cheapest we have seen it becomes the new cheapest (BUY THE DIP)
                 min_price = price
             else:
-                profit = price - min_price
+                profit = price - min_price   #otherwise the price is higher which means we need to calculate the profit if we sold today so simply put (todays price - or cheapest buy point) and if that profit beats our best so far it becomes the new best profit.
                 if profit > max_profit:
                     max_profit = profit
 
@@ -22,5 +22,5 @@ class Solution:
 
 
 
-solution = Solution()
-print(solution.Maximum_profit([7,1,5,3,6,4]))
+solution = Solution() #defining the solution class then passing in a test where the answer returned as 5 
+print(solution.Maximum_profit([7,1,5,3,6,4])) 
